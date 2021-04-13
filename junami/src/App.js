a1a5372a5d67c330fcb137529404ffcbba231cab
junamee <skawn11114@gmail.com>
2021-04-14 05:02:46 +0900

    Add. navBar LogStorage Animation

--- junami/src/App.js
+++ junami/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import styled from "styled-components";
-import TemplateHead from "./components/Templates/TemplateHead";
-import TemplateColumn from "./components/Templates/TemplateColumn";
+import StateProvider from "./components/StateProvider";
 import { TodoProvider } from "./components/Card/CardContext";
 import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
@@ -11,24 +9,17 @@ const GlobalStyle = createGlobalStyle`
     margin-right: 100px;
     font-family: Noto Sans KR;
     font-style: normal;
-  }
-`;
+    overflow:hidden;
 
-const TemplateBoard = styled.div`
-  display: flex;
+ 
+  }
 `;
 
 function App() {
   return (
     <TodoProvider>
       <GlobalStyle />
-      <TemplateHead />
-
-      <TemplateBoard>
-        <TemplateColumn>해야할 일</TemplateColumn>
-        <TemplateColumn>하고 있는 일</TemplateColumn>
-        <TemplateColumn>완료한 일</TemplateColumn>
-      </TemplateBoard>
+      <StateProvider />
     </TodoProvider>
   );
 }
--- junami/src/components/TypeSwitch.js
+++
@@ -1,6 +0,0 @@
-const switchType = (string) =>{
-  if(string ==="해야할 일")
-}
-if (children === "해야할 일") type = "todo";
-  if (children === "하고 있는 일") type = "doing";
-  if (children === "완료한 일") type = "done";
